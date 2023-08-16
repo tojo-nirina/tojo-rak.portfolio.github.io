e04191f3f01b7ca1dce83ce13743891a7eb77553
tojoniaina <exempletojoniaina@gmail.com>
2023-08-16 18:02:19 +0300

    ajout d'autre projet dans le fichier hthml

--- assets/index.js
+++ assets/index.js
@@ -4,3 +4,19 @@ let body = document.querySelector('body');
 toggle.addEventListener('click', function (){
     body.classList.toggle('open');
 })
+
+const allLink = document.querySelectorAll('.item-nav');
+allLink.forEach(item => {
+    item.addEventListener('click', () => {
+        body.classList.toggle('open');
+    })
+})
+
+const menuBtn = document.getElementById("menuBtn");
+const sideMenu = document.getElementById("sideMenu");
+const mainFlou = document.getElementById("main");
+
+menuBtn.addEventListener("click", () => {
+    sideMenu.classList.toggle("open");
+    mainFlou.classList.toggle('flou');
+})
